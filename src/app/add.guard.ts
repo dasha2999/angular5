64666ae3e5ee9fa23b3eecb4e6c96a91790da7aa
dasha2999 <125147552+dasha2999@users.noreply.github.com>
2023-11-20 21:13:56 +0300

    Add files via upload

---
+++ src/app/add.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const addGuard: CanActivateFn = (route, state) => {
+
+  const auth = inject(AuthService)
+  const router = inject(Router)
+  if (auth.isLoggedIn) {
+    return true
+  } else {
+    router.navigate(["login"])
+    return false
+  }
+};
